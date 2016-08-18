e0708ca628164de22bd20b6d43d50b75b29feeb0
Eeo Jun <141bytes@gmail.com>
2016-08-18 23:05:54 +0800

    fix bug + add client code

---
+++ client.py
@@ -0,0 +1,28 @@
+import struct
+import socket
+
+
+class RmbdClient:
+    def __init__(self, addr):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.addr = addr
+
+    def send(self, req):
+        self.socket.sendto(req, self.addr)
+
+    def add(self, key):
+        self.send(b'0' + key)
+
+    def count(self, key):
+        self.send(b'1' + key)
+        size = struct.calcsize('L140p')
+        count, key = struct.unpack('L140p', self.socket.recv(size))
+        if key == key:
+            return count
+
+    def sync(self, row, counters):
+        self.send(
+            b'2' +
+            struct.pack('H' + 'L'*len(counters),
+                *((row,) + tuple(counters)))
+        )
