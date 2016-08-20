53646192670b3fd1adf8136f753ac35545d6ca42
Eeo Jun <141bytes@gmail.com>
2016-08-20 20:35:08 +0800

    more tests for protocol

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -30,10 +30,20 @@ def test_parse_ack():
     assert req == (Type.ack, None)
 
 
-def test_parse_invalid():
-    assert not parse(b'\x00' + b'abc')
+def test_parse_tip_invalid():
     assert not parse(b'\x05')
     assert not parse(b'')
+
+
+def test_parse_payload_invalid():
+    for bit in range(5):
+        tip = chr(bit).encode()
+        data = b'random invalid data'
+        # we don't care about body of ack requests
+        if bit == 4:
+            assert parse(tip) == (Type.ack, None)
+            continue
+        assert not parse(tip + data)
 
 
 def test_bit():
